32e70e0c17c9685ce7b370d8d5fdf559aad85ea7
Everton Cunha <ever.cunha@gmail.com>
2018-03-06 16:00:37 -0300

    Carthage framework renamed to FJNSStringMask.

---
+++ FJNSStringMask/FJNSStringMask.h
@@ -0,0 +1,20 @@
+//
+//  FJNSStringMask.h
+//  FJNSStringMask
+//
+//  Created by Everton Cunha on 06/03/18.
+//  Copyright © 2018 Flavio Caetano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FJNSStringMask.
+FOUNDATION_EXPORT double FJNSStringMaskVersionNumber;
+
+//! Project version string for FJNSStringMask.
+FOUNDATION_EXPORT const unsigned char FJNSStringMaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FJNSStringMask/PublicHeader.h>
+
+#import "UITextFieldMask.h"
+#import "NSStringMask.h"
