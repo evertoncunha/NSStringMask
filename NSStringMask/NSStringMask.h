66f1e4be63ff0bf180fef57b91c4168e98fe2ff5
Everton Cunha <ever.cunha@gmail.com>
2018-03-06 15:32:31 -0300

    Carthage support.

---
+++ NSStringMask/NSStringMask.h
@@ -0,0 +1,19 @@
+//
+//  NSStringMask.h
+//  NSStringMask
+//
+//  Created by Everton Cunha on 06/03/18.
+//  Copyright © 2018 Flavio Caetano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSStringMask.
+FOUNDATION_EXPORT double NSStringMaskVersionNumber;
+
+//! Project version string for NSStringMask.
+FOUNDATION_EXPORT const unsigned char NSStringMaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSStringMask/PublicHeader.h>
+
+
